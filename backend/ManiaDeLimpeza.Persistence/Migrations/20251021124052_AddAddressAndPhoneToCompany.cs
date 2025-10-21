49cce5273e0172c83e0b1882fc1157028d701f8a
AndreSilva301 <145797488+AndreSilva301@users.noreply.github.com>
2025-10-21 09:44:02 -0300

    Expandir Entidade Company e  Implementar Role SystemAdmin

---
+++ backend/ManiaDeLimpeza.Persistence/Migrations/20251021124052_AddAddressAndPhoneToCompany.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ManiaDeLimpeza.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddressAndPhoneToCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Companies",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CNPJ",
+                table: "Companies",
+                type: "nvarchar(18)",
+                maxLength: 18,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_City",
+                table: "Companies",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Complement",
+                table: "Companies",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Neighborhood",
+                table: "Companies",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Number",
+                table: "Companies",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_State",
+                table: "Companies",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Street",
+                table: "Companies",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_ZipCode",
+                table: "Companies",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone_Landline",
+                table: "Companies",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone_Mobile",
+                table: "Companies",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address_City",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Complement",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Neighborhood",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Number",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Address_State",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Street",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Address_ZipCode",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Phone_Landline",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Phone_Mobile",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CNPJ",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(18)",
+                oldMaxLength: 18,
+                oldNullable: true);
+        }
+    }
+}
